3057af587a650561e8bf2e710873cd59252e5541
Matt Rose <mattroseinbox@gmail.com>
2020-09-14 09:18:01 +0430

    test: header of explorer

---
+++ src/app/pages/Explore/Header/__test__/Header.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Header from '../index'
+describe('Explorer<Header>', () => {
+    it('Trending', () => {
+        const wrapper = mount(<Header />)
+        expect(wrapper.find('h1').text()).toEqual('Trending')
+    })
+    it('Description', () => {
+        const wrapper = mount(<Header />)
+        expect(wrapper.find('p').text()).toEqual(
+            'See what the GitHub community is most excited about today.',
+        )
+    })
+})
