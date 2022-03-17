621edfb08ef2a63b1cb166981311dce46dc87bb1
Matt Rose <mattroseinbox@gmail.com>
2022-03-17 20:00:14 +0300

    fix: remove unused code on config

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,13 +1,5 @@
-export const PORT: number = 8000
-export const PROTOCOL: string = 'http'
-export const ENDPOINT: string = 'graphql'
-export const DOMAIN_URL = 'localhost'
-export const API_URL: string = `${PROTOCOL}://${DOMAIN_URL}:${PORT}/${ENDPOINT}`
+export const API_URL: string = process.env.REACT_APP_API_URL || ''
 
 export default {
-    PORT,
-    PROTOCOL,
-    ENDPOINT,
-    DOMAIN_URL,
     API_URL,
 }
